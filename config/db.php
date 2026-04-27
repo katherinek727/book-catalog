3c669b83f99190868d0a64af7e0d39f229da76d5
katherinek727 <katherinek727@outlook.com>
2026-04-27 06:57:59 +0300

    chore: configure db connection with local override support

--- config/db.php
+++ config/db.php
@@ -1,14 +1,21 @@
 <?php
 
-return [
+$db = [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=book_catalog',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+
+$localDb = __DIR__ . '/db-local.php';
+if (is_file($localDb)) {
+    $db = array_merge($db, require $localDb);
+}
+
+return $db;
