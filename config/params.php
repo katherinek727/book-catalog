dcaffd283ee869f0a0ca9dbc8aea69e2264defd9
katherinek727 <katherinek727@outlook.com>
2026-04-27 06:51:07 +0300

    chore: add params-local.php for local config overrides

--- config/params.php
+++ config/params.php
@@ -1,7 +1,14 @@
 <?php
 
-return [
+$params = [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
 ];
+
+$localParams = __DIR__ . '/params-local.php';
+if (is_file($localParams)) {
+    $params = array_merge($params, require $localParams);
+}
+
+return $params;
